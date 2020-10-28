7c70eef2bc77b32e9fdf98b82ab151c5fdadd1d2
Gabriel Pacheco <gabrielpacheco.wddigital@gmail.com>
2020-10-28 19:59:27 -0300

    Adicionando propriedades
    Aqui adicionamos propriedade nos componentes para que o a interface fique mais dinâmica.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  listTitles = [{
+    trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: 'assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia',
+    relevance: 98,
+    age: 0,
+    parts: 2,
+    categories: ['Épico', 'Filme de fantasia', 'Viagem no espaço']
+  }, {
+    trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: 'assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia 2',
+    relevance: 80,
+    age: 12,
+    parts: 2,
+    categories: ['Épico', 'Filme de fantasia', 'Viagem no espaço']
+  },{
+    trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: 'assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia 3',
+    relevance: 55,
+    age: 18,
+    parts: 2,
+    categories: ['Épico', 'Filme de fantasia', 'Viagem no espaço']
+  }];
   
 }
